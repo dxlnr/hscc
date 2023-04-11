1fe384be4f37b017f3cfcdf41dc5f02d2e6e8855
dxlnr <daniel.illner@outlook.de>
2023-04-11 23:51:33 +0200

    bits

--- main.hs
+++ main.hs
@@ -1,8 +1,20 @@
+---- Lexical Analysis
+----
+{-# LANGUAGE OverloadedStrings #-}
+module LexingFSM where
+
+import Control.Monad.IO.Class
+import qualified Data.Text.IO as T
 import System.Environment (getArgs)
 import System.IO (openFile, IOMode(ReadMode), hGetContents)
 import Data.Typeable (typeOf)
+import Control.Monad      (foldM)
+import Data.List.NonEmpty
+import Data.Text          (Text)
+import Text.Printf        (printf)
 
--- Tokens : Keywords, Operators, Strings, Constants, Special Characters, Identifiers.
+
+---- Tokens : Keywords, Operators, Strings, Constants, Special Characters, Identifiers.
 data Token = Key | Ops | Str | Const | SChar | Ident
 
 instance Show Token where 
@@ -13,32 +25,51 @@ instance Show Token where
     show SChar = "Special Character"
     show Ident = "Identifier"
 
--- let keys :: [String] -> 
--- Lexer
-lexer :: Char -> Token
-lexer c 
-    | (c == ','|| c == '.'|| c == ';'|| c ==':'|| c =='?'|| c =='*') = SChar
-    | otherwise = Str 
+-- -- Lexer
+-- lexer :: Char -> Token
+-- lexer c 
+--     | (c == ','|| c == '.'|| c == ';'|| c ==':'|| c =='?'|| c =='*') = SChar
+--     | otherwise = Str 
 
--- Iterate over every single character.
-l c = [ lexer x | x <- c ]
+-- -- Iterate over every single character.
+-- l c = [ lexer x | x <- c ]
 
--- FSM 
+-- main = do
+--     args <- getArgs
+--     file <- openFile (head args) ReadMode
+--     prog <- hGetContents file
+--     print prog
+--     print (typeOf prog)
+--     print (l prog)
+--     print (length (l prog))
+
+
+data LexingState 
+    = Init 
+    | ReturnToken 
+    deriving (Show, Eq)
+
+data LexingEvent 
+    = GetToken
+    deriving (Show, Eq)
+
+type FSM s e = s -> e -> IO s
+lexer :: FSM LexingState LexingEvent 
+
+-- lexer Init (GetToken token) =
+--   return (ReturnToken (token :| []))
 --
--- States
-data Start deriving (Show, Eq)
-data Identifier (Show, Eq)
-data Terminating (Show, Eq)
 
--- Events
-data Letter m = ReadDataFromEnv (State m Identifier)
-  -- deriving (Show, Eq)
+lexer state _ = return state
 
-main = do
-    args <- getArgs
-    file <- openFile (head args) ReadMode
-    prog <- hGetContents file
-    print prog
-    print (typeOf prog)
-    print (l prog)
-    print (length (l prog))
+runFsm :: Foldable f => FSM s e -> s -> f e -> IO s
+runFsm = foldM
+
+withLogging
+  :: (Show s, Show e)
+  => FSM s e
+  -> FSM s e
+withLogging fsm s e = do
+  s' <- fsm s e
+  printf "- %s × %s → %s\n" (show s) (show e) (show s')
+  return s'
