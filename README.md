eaeee2587646e8cb65a5607783cfaa5d88c4d5b1
dxlnr <daniel.illner@outlook.de>
2023-08-14 15:45:01 +0200

    upd

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 C Compiler 
 
-First task is to implement a lexer using a finite state machine.
+## Overview
 
+```bash
+Lexing -> Parsing -> Static Analysis -> Intermediate Representation -> Optimization -> Code Generation
+```
 
 ## Prerequisits 
 
@@ -22,6 +25,78 @@ ghc main.hs -o hscc && ./hscc
 For the **c** version run
 ```bash 
 make main && ./main tests/simple.c
+```
+
+### 
+### Token Types
+
+```cpp
+class Token {
+public:
+  enum Kind {
+    /// Markers.
+    eof,
+    error,
+    /// Token signifying a code completion location.
+    code_complete,
+    /// Token signifying a code completion location within a string.
+    code_complete_string,
+
+    /// Keywords.
+    KW_BEGIN,
+    /// Dependent keywords, i.e. those that are treated as keywords depending on
+    /// the current parser context.
+    KW_DEPENDENT_BEGIN,
+    kw_attr,
+    kw_op,
+    kw_type,
+    KW_DEPENDENT_END,
+
+    /// General keywords.
+    kw_Attr,
+    kw_erase,
+    kw_let,
+    kw_Constraint,
+    kw_Op,
+    kw_OpName,
+    kw_Pattern,
+    kw_replace,
+    kw_return,
+    kw_rewrite,
+    kw_Rewrite,
+    kw_Type,
+    kw_TypeRange,
+    kw_Value,
+    kw_ValueRange,
+    kw_with,
+    KW_END,
+
+    /// Punctuation.
+    arrow,
+    colon,
+    comma,
+    dot,
+    equal,
+    equal_arrow,
+    semicolon,
+    /// Paired punctuation.
+    less,
+    greater,
+    l_brace,
+    r_brace,
+    l_paren,
+    r_paren,
+    l_square,
+    r_square,
+    underscore,
+
+    /// Tokens.
+    directive,
+    identifier,
+    integer,
+    string_block,
+    string
+  };
 ```
 
 ## Additional Information
--- lexer.c
+++ lexer.c
@@ -5,41 +5,41 @@
 
 char *read_file(char *fname) 
 {
-    FILE *file = NULL;
-    file = fopen(fname, "r");
+  FILE *file = NULL;
+  file = fopen(fname, "r");
 
-    if (file == NULL) return NULL;
+  if (file == NULL) return NULL;
 
-    fseek(file, 0, SEEK_END);
-    int length = ftell(file);
-    fseek(file, 0, SEEK_SET);
+  fseek(file, 0, SEEK_END);
+  int length = ftell(file);
+  fseek(file, 0, SEEK_SET);
 
-    char *s = malloc(sizeof(char) * (length + 1)); 
-    char c;
-    int i = 0;
-    while( (c = fgetc(file)) != EOF)
-    {
-        s[i] = c;
-        i++;
-    }
-    s[i] = '\0';
+  char *s = malloc(sizeof(char) * (length + 1)); 
+  char c;
+  int i = 0;
+  while( (c = fgetc(file)) != EOF)
+  {
+      s[i] = c;
+      i++;
+  }
+  s[i] = '\0';
 
-    fclose(file);
+  fclose(file);
 
-    return s;
+  return s;
 }
 
 char* substr(const char *src, int m, int n)
 {
-    int len = n-m;
-    char *dest = (char*) malloc(sizeof(char)*(len + 1));
-    for (int i=m; i<n && (*(src+i) != '\0'); i++)
-    {
-        *dest = *(src + i);
-        dest++;
-    }
-    *dest = '\0';
-    return (dest - len);
+  int len = n-m;
+  char *dest = (char*) malloc(sizeof(char)*(len + 1));
+  for (int i=m; i<n && (*(src+i) != '\0'); i++)
+  {
+      *dest = *(src + i);
+      dest++;
+  }
+  *dest = '\0';
+  return (dest - len);
 }
 
 int strin(char **arr, int len, char *s) {
@@ -52,239 +52,247 @@ int strin(char **arr, int len, char *s) {
 }
 
 typedef enum states {
-    START = 0,
-    DIGIT = 1,
-    LETTER = 2, 
-    OPS = 3,
-    STR = 4,
+  START = 0,
+  DIGIT = 1,
+  LETTER = 2, 
+  OPS = 3,
+  STR = 4,
 } states_t;
 
 char *keyword_str [] = {
-    "auto", 
-    "double", 
-    "int", 
-    "struct",
-    "break", 
-    "else",  
-    "long",
-    "switch",
-    "case",
-    "enum",
-    "register",  
-    "typedef",
-    "char", 
-    "extern",     
-    "return",    
-    "union",
-    "const",
-    "float",       
-    "short",      
-    "unsigned",
-    "continue",  
-    "for",         
-    "signed",   
-    "void",
-    "default",      
-    "goto",        
-    "sizeof",     
-    "volatile",
-    "do",         
-    "if", 
-    "static",    
-    "while",
+  "auto", 
+  "double", 
+  "int", 
+  "struct",
+  "break", 
+  "else",  
+  "long",
+  "switch",
+  "case",
+  "enum",
+  "register",  
+  "typedef",
+  "char", 
+  "extern",     
+  "return",    
+  "union",
+  "const",
+  "float",       
+  "short",      
+  "unsigned",
+  "continue",  
+  "for",         
+  "signed",   
+  "void",
+  "default",      
+  "goto",        
+  "sizeof",     
+  "volatile",
+  "do",         
+  "if", 
+  "static",    
+  "while",
 };
 
 typedef enum token_type {
-    ident = 0,
-    keyword = 1,
-    integer = 2,
-    str = 3,
-    schar = 4,
-    ops = 5,
+  ident = 0,
+  keyword = 1,
+  integer = 2,
+  str = 3,
+  schar = 4,
+  ops = 5,
 } token_type_t;
 
 typedef struct token {
-    token_type_t t;
-    char *s; 
-    int ptr_l;
+  token_type_t t;
+  char *s; 
+  int ptr_l;
 } token_t;
 
 const char * const token_str[] =
 {
-    [ident] = "IDENT",
-    [keyword] = "KEYWORD",
-    [integer]  = "INTEGER",
-    [str]  = "STRING",
-    [schar]  = "SPECIAl CHAR",
-    [ops]  = "OPERATOR",
+  [keyword] = "KEYWORD",
+  [ident] = "ident",
+  [integer]  = "integer",
+  [str]  = "string",
+  [schar]  = "SPECIAl CHAR",
+  [ops]  = "OPERATOR",
 };
 
 void stdout_token(token_t *token) {
-    printf("  [(%s, %d), %.*s]\n", token_str[token->t], token->t, token->ptr_l, token->s);
+  printf("  [(%s, %d), %.*s]\n", token_str[token->t], token->t, token->ptr_l, token->s);
 }
 
 typedef struct tokens {
-    token_t t;
-    struct tokens * next;
+  token_t t;
+  struct tokens * next;
 } tokens_t;
 
 void append_token(tokens_t **head_ref, token_t token) {
-    tokens_t * n_token = (tokens_t *) malloc(sizeof(tokens_t));
-    n_token->t = token;
+  tokens_t * n_token = (tokens_t *) malloc(sizeof(tokens_t));
+  n_token->t = token;
 
-    tokens_t* last = *head_ref;
-    n_token->next = NULL;
+  tokens_t* last = *head_ref;
+  n_token->next = NULL;
 
-    if (*head_ref == NULL) {
-        *head_ref = n_token;
-        return;
-    }
-    while (last->next != NULL) {
-        last = last->next;
-    }
-    last->next = n_token;
+  if (*head_ref == NULL) {
+    *head_ref = n_token;
+    return;
+  }
+  while (last->next != NULL) {
+    last = last->next;
+  }
+  last->next = n_token;
 }
 
 void push(tokens_t **head_ref, token_t token)
 {
-    tokens_t * n_token = (tokens_t *) malloc(sizeof(tokens_t));
-    n_token->t = token;
-    n_token->next = (*head_ref);
-    (*head_ref) = n_token;
+  tokens_t * n_token = (tokens_t *) malloc(sizeof(tokens_t));
+  n_token->t = token;
+  n_token->next = (*head_ref);
+  (*head_ref) = n_token;
 }
 
 void show_tokens(tokens_t *tokens) {
-    while (tokens != NULL) {
-        stdout_token(&tokens->t);
-        tokens = tokens->next;
-    }
+  while (tokens != NULL) {
+    stdout_token(&tokens->t);
+    tokens = tokens->next;
+  }
 }
 
 tokens_t *lexical_analysis(char *s) {
-    token_t t;
-    tokens_t *tokens = NULL;
-    states_t state = START;
+  token_t t;
+  tokens_t *tokens = NULL;
+  states_t state = START;
 
-    int dc = 0;
-    int lc = 0;
-    int ops_c = 0;
-    int str_c = 0;
+  int dc = 0;
+  int lc = 0;
+  int ops_c = 0;
+  int str_c = 0;
 
-    for (int i = 0; i < strlen(s); ++i) {
-        switch(state) {
-            case START:
-                if (isdigit(s[i])) {
-                    state = DIGIT;
-                    dc++;
-                } else if (isalpha(s[i])) {
-                    state = LETTER;
-                    lc++;
-                } else if ( 
-                 ( s[i] == '+' ) || ( s[i] == '-' ) ||
-                 ( s[i] == '/' ) || ( s[i] == '*' ) ||
-                 ( s[i] == '%' ) || ( s[i] == '=' ) ||
-                 ( s[i] == '<' ) || ( s[i] == '>' ) ||
-                 ( s[i] == '&' ) || ( s[i] == '^' ) ||
-                 ( s[i] == '|' ) || ( s[i] == '!' ))
-                { 
-                    state = OPS;
-                    ops_c++;
-                } else if (
-                   ( s[i] == '[' ) || ( s[i] == ']' ) || 
-                   ( s[i] == '(' ) || ( s[i] == ')' ) ||
-                   ( s[i] == '{' ) || ( s[i] == '}' ) ||
-                   ( s[i] == ',' ) || ( s[i] == ';' ) || 
-                   ( s[i] == ':' ) || ( s[i] == '~' ) ||
-                   ( s[i] == '.' ) || ( s[i] == '#' ))
-                {
-                    t  = (token_t) { .t = schar, .s = &s[i], .ptr_l = 1 };
-                    append_token(&tokens, t);
-                } else if ( s[i] == '"' ) 
-                {
-                    state = STR;
-                    str_c++;
-                }               
-                break;
-            case DIGIT:
-                if (isdigit(s[i])) {
-                    dc++;
-                } else {
-                    t = (token_t) { .t = integer, .s = &s[i - dc], .ptr_l = dc };
-                    append_token(&tokens, t);
-                    state = START;
-                    dc = 0;
-                    i--;
-                }
-                break;
-            case LETTER:
-                if ( isalpha(s[i]) || isdigit(s[i]) || ( s[i] == '_' ) ) {
-                    lc++;
-                } else {
-                    char* dest = substr(s, i - lc, i);
-
-                    if (strin(keyword_str, 32, dest)) {
-                        t = (token_t) { .t = keyword, .s = &s[i - lc], .ptr_l = lc };
-                    } else {
-                        t = (token_t) { .t = ident, .s = &s[i - lc], .ptr_l = lc };
-                    }
-                    append_token(&tokens, t);
-                    state = START;
-                    lc = 0;
-                    i--;
-                }
-                break;
-            case OPS:
-                if ( 
-                 ( s[i] == '+' ) || ( s[i] == '-' ) ||
-                 ( s[i] == '/' ) || ( s[i] == '*' ) ||
-                 ( s[i] == '%' ) || ( s[i] == '=' ) ||
-                 ( s[i] == '<' ) || ( s[i] == '>' ) ||
-                 ( s[i] == '&' ) || ( s[i] == '^' ) ||
-                 ( s[i] == '|' ) || ( s[i] == '!' ))
-                {
-                    ops_c++;
-                } else {
-                    token_t t = { .t = ops, .s = &s[i - ops_c], .ptr_l = ops_c };
-                    append_token(&tokens, t);
-                    state = START;
-                    ops_c = 0;
-                    i--;
-                }
-                break;
-            case STR:
-                if (( s[i] != '"' ) && ( s[i] != '\n' )) 
-                {
-                    str_c++;
-                } else {
-                    token_t t = { .t = str, .s = &s[i - str_c], .ptr_l = str_c + 1 };
-                    append_token(&tokens, t);
-                    state = START;
-                    str_c = 0;
-                }
-                break;
-            default:
-                state = START;
+  for (int i = 0; i < strlen(s); ++i) {
+    switch(state) {
+      case START:
+        if (isdigit(s[i])) {
+          state = DIGIT;
+          dc++;
+        } else if (isalpha(s[i])) {
+          state = LETTER;
+          lc++;
+        } else if ( 
+          ( s[i] == '+' ) || ( s[i] == '-' ) ||
+          ( s[i] == '/' ) || ( s[i] == '*' ) ||
+          ( s[i] == '%' ) || ( s[i] == '=' ) ||
+          ( s[i] == '<' ) || ( s[i] == '>' ) ||
+          ( s[i] == '&' ) || ( s[i] == '^' ) ||
+          ( s[i] == '|' ) || ( s[i] == '!' ))
+        {
+          state = OPS;
+          ops_c++;
+        } else if (
+          ( s[i] == '[' ) || ( s[i] == ']' ) || 
+          ( s[i] == '(' ) || ( s[i] == ')' ) ||
+          ( s[i] == '{' ) || ( s[i] == '}' ) ||
+          ( s[i] == ',' ) || ( s[i] == ';' ) || 
+          ( s[i] == ':' ) || ( s[i] == '~' ) ||
+          ( s[i] == '.' ) || ( s[i] == '#' ))
+        {
+          t  = (token_t) { .t = schar, .s = &s[i], .ptr_l = 1 };
+          append_token(&tokens, t);
+        } else if ( s[i] == '"' ) 
+        {
+          state = STR;
+          str_c++;
+        }               
+        break;
+      case DIGIT:
+        if (isdigit(s[i])) {
+          dc++;
+        } else {
+          t = (token_t) { .t = integer, .s = &s[i - dc], .ptr_l = dc };
+          append_token(&tokens, t);
+          state = START;
+          dc = 0;
+          i--;
         }
+        break;
+      case LETTER:
+        if ( isalpha(s[i]) || isdigit(s[i]) || ( s[i] == '_' ) ) {
+          lc++;
+        } else {
+              char* dest = substr(s, i - lc, i);
+
+              if (strin(keyword_str, 32, dest)) {
+                  t = (token_t) { .t = keyword, .s = &s[i - lc], .ptr_l = lc };
+              } else {
+                  t = (token_t) { .t = ident, .s = &s[i - lc], .ptr_l = lc };
+              }
+              append_token(&tokens, t);
+              state = START;
+              lc = 0;
+              i--;
+        }
+        break;
+      case OPS:
+        if ( 
+          ( s[i] == '+' ) || ( s[i] == '-' ) ||
+          ( s[i] == '/' ) || ( s[i] == '*' ) ||
+          ( s[i] == '%' ) || ( s[i] == '=' ) ||
+          ( s[i] == '<' ) || ( s[i] == '>' ) ||
+          ( s[i] == '&' ) || ( s[i] == '^' ) ||
+          ( s[i] == '|' ) || ( s[i] == '!' ))
+        {
+          ops_c++;
+        } else {
+          token_t t = { .t = ops, .s = &s[i - ops_c], .ptr_l = ops_c };
+          append_token(&tokens, t);
+          state = START;
+          ops_c = 0;
+          i--;
+        }
+        break;
+      case STR:
+        if (( s[i] != '"' ) && ( s[i] != '\n' )) 
+        {
+          str_c++;
+        } else {
+          token_t t = { .t = str, .s = &s[i - str_c], .ptr_l = str_c + 1 };
+          append_token(&tokens, t);
+          state = START;
+          str_c = 0;
+        }
+        break;
+      default:
+        state = START;
+      }
     }
     return tokens;
 }
 
+typedef enum ast_type {
+  AST_NUMBER,
+  AST_ADD,
+  AST_MUL,
+
+} ast_type_t;
+
+
 int main(int argc, char **argv)
 {
-    char *fc = read_file(argv[1]);
-    if (fc == NULL)
-    {
-        printf("Error reading file.\n");
-        return 1;
-    }
+  char *fc = read_file(argv[1]);
+  if (fc == NULL)
+  {
+      printf("Error reading file.\n");
+      return 1;
+  }
 
-    tokens_t *tokens = lexical_analysis(fc);
+  tokens_t *tokens = lexical_analysis(fc);
 
-    // Show the results to the console.
-    printf("Tokens: \n");
-    show_tokens(tokens);
+  // Show the results to the console.
+  printf("Tokens: \n");
+  show_tokens(tokens);
 
-    free(fc);
+  free(fc);
 
-    return 0;
+  return 0;
 }
